dcde8e7e7e676065dc3fcf326eb1b83011dca640
Pbonnamy <bonnamy.77@gmail.com>
2022-11-08 12:58:53 +0100

    better set item text & fix

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -38,28 +38,27 @@ class MainActivity : AppCompatActivity() {
             listOf("Petits pois 66%", "eau", "garniture 2,8% (salade, oignon grelot)", "sucre", "sel", "arôme naturel"),
         )
 
-        setItemText(R.id.product_name, R.string.product_name, product.name)
-        setItemText(R.id.brand, R.string.brand, product.brand)
-        setItemText(R.id.bar_code, R.string.bar_code, product.bar_code)
-        setItemText(R.id.quantity, R.string.quantity, product.quantity)
-
-        setItemText(R.id.ingredients, R.string.ingredients, product.ingredients.joinToString(", "))
-        setItemText(R.id.allergen, R.string.allergens, product.allergens.joinToString(", "))
-        setItemText(R.id.additives, R.string.additives, product.additives.joinToString(", "))
-        setItemText(R.id.sold_locations, R.string.sold_locations, product.sold_locations.joinToString(", "))
+        findViewById<TextView>(R.id.product_name).applyBoldText(product.name, R.string.product_name)
+        findViewById<TextView>(R.id.brand).applyBoldText(product.brand, R.string.brand)
+        findViewById<TextView>(R.id.bar_code).applyBoldText(product.bar_code, R.string.bar_code)
+        findViewById<TextView>(R.id.quantity).applyBoldText(product.quantity, R.string.quantity)
+        findViewById<TextView>(R.id.sold_locations).applyBoldText(product.sold_locations.joinToString(", "), R.string.sold_locations)
+        findViewById<TextView>(R.id.ingredients).applyBoldText(product.ingredients.joinToString(", "), R.string.ingredients)
+        findViewById<TextView>(R.id.allergens).applyBoldText(product.allergens.joinToString(", "), R.string.allergens)
+        findViewById<TextView>(R.id.additives).applyBoldText(product.additives.joinToString(", "), R.string.additives)
 
         findViewById<ImageView>(R.id.nutriscore).setImageResource(product.getNutriscoreDrawable())
 
         Glide.with(this).load(product.img).into(findViewById(R.id.product_img))
     }
 
-    private fun setItemText(idView: Int, idString: Int, text: String) {
+    private fun TextView.applyBoldText(text: String, idString: Int) {
         val string = getString(idString, text.ifEmpty { "Ø" })
         val spannable = SpannableString(string)
         if (string.indexOf(":") != -1) {
             spannable.setSpan(StyleSpan(Typeface.BOLD), 0, string.indexOf(":") + 1, 0)
         }
-
-        findViewById<TextView>(idView).text = spannable
+        this.text = spannable
     }
+
 }
